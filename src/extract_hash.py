fe5e7ee537fcce1e0b30f8f855545a5ce1c0e575
tushan-shahid <shahidshabraj@gmail.com>
2023-03-08 19:49:50 -0800

    fixes

--- src/extract_hash.py
+++ src/extract_hash.py
@@ -1,33 +1,34 @@
 import requests
+import concurrent.futures
 
-print('Downloading…')
+def download_hash_file(id):
+    url = f'https://virusshare.com/hashfiles/VirusShare_{str(id).zfill(5)}.md5'
+    try:
+        response = requests.get(url, stream=True)
+        response.encoding = 'utf-8'
+        return response.text
+    except requests.exceptions.HTTPError as error:
+        if error.response.status_code == 404:
+            return None
+        raise error
 
-with open('virushashes.txt', 'w') as virushashes:
-	isFirstLine = True
+if __name__ == '__main__':
+    print('Downloading...')
+    with open('virushashes.txt', 'w') as virushashes_file:
+        isFirstLine = True
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(download_hash_file, id) for id in range(99999)]
+            for future in concurrent.futures.as_completed(futures):
+                hash_file = future.result()
+                if hash_file is not None:
+                    for line in hash_file.splitlines():
+                        if line.startswith('#'):
+                            continue
+                        if isFirstLine:
+                            isFirstLine = False
+                        else:
+                            line = '\n' + line
+                        virushashes_file.write(line)
+                print(f'Downloaded hash file {id}.md5')
 
-	for id in range(99999):
-		url = f'https://virusshare.com/hashfiles/VirusShare_{str(id).zfill(5)}.md5'
-
-		try:
-			response = requests.get('https://virusshare.com/hashfiles/VirusShare_00000.md5', stream=True)
-			response.encoding = 'utf-8'
-
-			for line in response.iter_lines(decode_unicode=True):
-				if line.startswith('#'):
-					continue
-
-				if isFirstLine:
-					isFirstLine = False
-				else:
-					line = '\n' + line
-
-				virushashes.write(line)
-
-			print(f'Downloaded {url}')	
-		except requests.exceptions.HTTPError as error:
-			if error.response.status_code == 404:
-				break
-
-			raise error
-
-print('Download complete')
+    print('Download complete')
